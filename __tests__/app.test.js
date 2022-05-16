04408344061d1d393ec823cbccbdbbab127367cb
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-16 13:39:07 +0100

    fix the test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,26 +10,37 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 
-describe("1- getCategories() GET /api/categories", () => {
+describe("getCategories() GET /api/categories", () => {
   test("getCategories responds with status 200 and returns an object of arrays of categories", () => {
     return request(app)
       .get("/api/categories")
       .expect(200)
       .then(({ body }) => {
         expect(Array.isArray(body.categories)).toBe(true);
-        // console.log(body.categories)
         expect(body.categories.length).toBe(4);
-        body.categories.forEach((category)=>{
+      });
+  });
+  test("getCategories returns correct object keys and values", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.categories[0].description).toBe(
+          "Abstact games that involve little luck"
+        );
+        expect(body.categories[0].slug).toBe("euro game");
+        body.categories.forEach((category) => {
           expect(category).toEqual(
             expect.objectContaining({
               slug: expect.any(String),
-              description: expect.any(String)
+              description: expect.any(String),
             })
           );
-        })
+        });
       });
   });
-})
+});
+
 
 test("status:404, responds with the correct error message when passed a valid but non-existant endpoint", () => {
   return request(app)
