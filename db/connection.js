78c4cec4f95388b032e12672fc0d2a005347dfd5
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-19 11:22:20 +0100

    deploy

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,25 @@
 const { Pool } = require('pg');
+const path = require("path");
+
+
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+  path: path.resolve(__dirname, `../.env.${ENV}`)
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+module.exports = new Pool(config);
